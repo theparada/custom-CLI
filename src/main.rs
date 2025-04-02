a75f87e3a3b97e2579a759261efeef131c0d06d4
theparada <prangch.parada@gmail.com>
2025-04-02 21:28:51 +0200

    add print line number

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,12 @@ fn main() -> Result<()> {
     // get content + take care of error
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
-
+    let mut i: u32 = 0;
     // search for line with pattern
     for line in content.lines() {
+        i += 1;
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            println!("line {num}: {line}", num = i, line = line);
         }
     }
     Ok(())
